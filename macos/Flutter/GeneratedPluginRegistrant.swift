56e5c941508ec03e26e2e6206fcf6ab0c22aa508
Danial M. <danial@danials.org>
2025-12-25 22:04:27 +0330

    feat: Register `v2ray_dan` plugin in `GeneratedPluginRegistrant.swift`.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,11 +8,13 @@ import Foundation
 import package_info_plus
 import shared_preferences_foundation
 import url_launcher_macos
+import v2ray_dan
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  V2RayDanPlugin.register(with: registry.registrar(forPlugin: "V2RayDanPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
